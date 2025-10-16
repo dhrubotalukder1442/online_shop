72946c76ceebfbc53bc46461f973fcb71ec169e3
Zisan Provat <zisanprovat@gmail.com>
2025-10-16 14:39:12 +0600

    added authentication

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\TagController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\TagController;
+use Illuminate\Support\Facades\Route;
 
-Route::resource('products', ProductController::class);
-Route::resource('categories', CategoryController::class);
-Route::resource('tags', TagController::class);
-Route::resource('orders', OrderController::class);
-Route::resource('order-items', OrderItemController::class);
-Route::resource('reviews', ReviewController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::resource('products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('tags', TagController::class);
+    Route::resource('orders', OrderController::class);
+    Route::resource('order-items', OrderItemController::class);
+    Route::resource('reviews', ReviewController::class);
+});
+
+require __DIR__.'/auth.php';
